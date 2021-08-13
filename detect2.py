f076587ce2366d3659686a54f8e77d2d929d1f44
CesarBazan <cesar.bazan.c@gmail.com>
2021-08-12 20:43:45 -0500

    adding lib tabulate

--- detect2.py
+++ detect2.py
@@ -22,7 +22,7 @@ from utils.general import check_img_size, check_requirements, check_imshow, colo
     apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_sync
-
+from tabulate import tabulate
 
 @torch.no_grad()
 def run(weights='yolov5s.pt',  # model.pt path(s)
