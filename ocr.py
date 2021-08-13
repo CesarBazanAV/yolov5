6bd3def99704abb21ae735f08791ff362da3df01
CesarBazan <cesar.bazan.c@gmail.com>
2021-08-12 21:38:19 -0500

    adding ocr.py

---
+++ ocr.py
@@ -0,0 +1,37 @@
+# import PIL
+# from PIL import ImageDraw
+import easyocr
+
+imageLocation = 'datasets/TrafficPanels/images/IMG_4824.JPG'
+
+for bound in bounds:
+    print(bound)
+
+
+def run(source='data/images'  # file/dir/URL/glob, 0 for webcam
+        ):
+    bounds = reader.readtext(source)
+    for bound in bounds:
+        print(bound)
+
+
+def load_model():
+    reader = easyocr.Reader(['es', 'en'])  # need to run only once to load model into memory
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, default='data/images', help='file/dir/image')
+    opt = parser.parse_args()
+    return opt
+
+
+def main(opt):
+    print(colorstr('detect: ') + ', '.join(f'{k}={v}' for k, v in vars(opt).items()))
+    # check_requirements(exclude=('tensorboard', 'thop'))
+    run(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
