4a6dbfeda8e54681a3e402180479fda66d785d98
CesarBazan <cesar.bazan.c@gmail.com>
2021-10-07 20:42:37 -0500

    Adding OCR

--- detect3.py
+++ detect3.py
@@ -286,7 +286,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
     # Print results
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-    print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+    print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, OCR time %.fms per image at shape {(1, 3, *imgsz)}' % t)
     table = [['Activity', 'Time', 'Unit'],
              ['Pre-process', t[0], 'ms'],
              ['Inference', t[1], 'ms'],
